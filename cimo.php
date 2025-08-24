420d0cd0efdc8965ef9383422cd89c340cb254d6
bfintal@gmail.com <>
2025-08-24 13:38:41 +0800

    added license + some files

--- cimo.php
+++ cimo.php
@@ -3,14 +3,14 @@
  * Plugin Name: Cimo
  * Plugin URI: https://wpcimo.com/
  * Description: Convert images to WebP on upload.
- * Version: 0.1.0
- * Author: Benjamin Intal
- * Author URI: https://example.com/
- * License: GPL-2.0-or-later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Version: 1.0.0
+ * Author: Gambit Technologies, Inc
+ * Author URI: http://gambit.ph
  * Requires at least: 6.5
  * Requires PHP: 7.4
  * Text Domain: cimo
+ * 
+ * @fs_premium_only /freemius.php, /freemius/
  */
 
 // Exit if accessed directly.
