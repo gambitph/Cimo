97c5b9c288dc35f77110f70c5d4785b43562953e
bfintal@gmail.com <>
2025-08-25 14:27:48 +0800

    added support for Beaver Builder

--- src/admin/class-script-loader.php
+++ src/admin/class-script-loader.php
@@ -15,10 +15,24 @@ if ( ! class_exists( 'Cimo_Script_Loader' ) ) {
 			add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_media_assets' ] );
 			// Enqueue for Elementor.
 			add_action( 'elementor/editor/before_enqueue_scripts', [ $this, 'enqueue_media_assets' ] );
+			// Enqueue for Beaver Builder main window (outside BB's iframe).
+			if ( class_exists( 'FLBuilderModel' ) ) {
+				add_action( 'wp_head', [ $this, 'maybe_enqueue_for_beaver_builder' ], 999 );
+			}
 			// Enqueue for the admin area in general.
 			add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_media_assets' ] );
 		}
 
+		/**
+		 * Enqueue scripts in the main window when Beaver Builder is active.
+		 */
+		public function maybe_enqueue_for_beaver_builder() {
+			// Check if we're in the Beaver Builder editor context.
+			if ( class_exists( 'FLBuilderModel' ) && FLBuilderModel::is_builder_active() ) {
+				$this->enqueue_media_assets();
+			}
+		}
+		
 		public function enqueue_media_assets() {
 			// If cimo-editor is already enqueued, don't enqueue again.
 			if ( wp_script_is( 'cimo-editor', 'enqueued' ) ) {
@@ -33,7 +47,8 @@ if ( ! class_exists( 'Cimo_Script_Loader' ) ) {
 			wp_enqueue_script(
 				'cimo-editor',
 				$build_url . 'index.js',
-				$script_asset['dependencies'],
+				// Remove wp-dom-ready since it's not really a dependency, but wp-scripts includes it.
+				array_values( array_diff( $script_asset['dependencies'], [ 'wp-dom-ready' ] ) ),
 				$script_asset['version'],
 				true
 			);
