792116b5c7ddfe8b5ba2fd8368c2e34bb36feb73
bfintal <bfintal@gmail.com>
2026-07-17 12:27:49 +0800

    change order of upsell

--- src/admin/js/page/admin-settings.js
+++ src/admin/js/page/admin-settings.js
@@ -1231,18 +1231,6 @@ const AdminSettings = () => {
 						<ul className="cimo-premium-features-list">
 							<li>
 								<span className="cimo-premium-icon">
-									{ /* Sparkles Icon */ }
-									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles-icon lucide-sparkles"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z" /><path d="M20 3v4" /><path d="M22 5h-4" /><path d="M4 17v2" /><path d="M5 18H3" /></svg>
-								</span>
-								<h4>
-									{ __( 'Smart Optimization', 'cimo-image-optimizer' ) }
-								</h4>
-								<span>
-									{ __( 'Automatically apply the best format and compression for every image and get smaller file sizes', 'cimo-image-optimizer' ) }
-								</span>
-							</li>
-							<li>
-								<span className="cimo-premium-icon">
 									{ /* Images Icon */ }
 									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-images-icon lucide-images"><path d="m22 11-1.296-1.296a2.4 2.4 0 0 0-3.408 0L11 16" /><path d="M4 8a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2" /><circle cx="13" cy="7" r="1" fill="currentColor" /><rect x="8" y="2" width="14" height="14" rx="2" /></svg>
 								</span>
@@ -1251,6 +1239,18 @@ const AdminSettings = () => {
 								</h4>
 								<span>
 									{ __( 'Optimize your existing files in your entire media library', 'cimo-image-optimizer' ) }
+								</span>
+							</li>
+							<li>
+								<span className="cimo-premium-icon">
+									{ /* Sparkles Icon */ }
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles-icon lucide-sparkles"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z" /><path d="M20 3v4" /><path d="M22 5h-4" /><path d="M4 17v2" /><path d="M5 18H3" /></svg>
+								</span>
+								<h4>
+									{ __( 'Smart Optimization', 'cimo-image-optimizer' ) }
+								</h4>
+								<span>
+									{ __( 'Automatically apply the best format and compression for every image and get smaller file sizes', 'cimo-image-optimizer' ) }
 								</span>
 							</li>
 							<li>
